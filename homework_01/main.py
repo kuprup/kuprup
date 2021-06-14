66a9edc49e01564ec736778461b3d6aa3fcad69b
Kuprup <55106367+kuprup@users.noreply.github.com>
2021-06-14 19:25:10 +0300

    add function first ok

--- homework_01/main.py
+++ homework_01/main.py
@@ -13,4 +13,19 @@ def power_numbers(n):
 print(power_numbers([1,2,3]))
 
 
+# filter types
+ODD = "odd"
+EVEN = "even"
+PRIME = "prime"
 
+
+def filter_numbers():
+    """
+    функция, которая на вход принимает список из целых чисел,
+    и возвращает только чётные/нечётные/простые числа
+    (выбор производится передачей дополнительного аргумента)
+    >>> filter_numbers([1, 2, 3], ODD)
+    <<< [1, 3]
+    >>> filter_numbers([2, 3, 4, 5], EVEN)
+    <<< [2, 4]
+    """
