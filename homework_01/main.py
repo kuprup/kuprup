72250c23619ab131c7647c2d7e6bdcfad4cbeed8
Kuprup <55106367+kuprup@users.noreply.github.com>
2021-06-15 14:12:53 +0300

    add function first ok

--- homework_01/main.py
+++ homework_01/main.py
@@ -4,13 +4,13 @@
 """
 
 
-def power_numbers(n):
+def power_numbers(*n):
     k=[]
     for i in n:
         a=i**2
         k.append(a)
     return k
-print(power_numbers([1,2,3]))
+#print(power_numbers(1,2,3))
 
 #rint(power_numbers(2))
 
@@ -21,13 +21,23 @@ EVEN = "even"
 PRIME = "prime"
 
 
-def filter_numbers():
-    """
-    функция, которая на вход принимает список из целых чисел,
-    и возвращает только чётные/нечётные/простые числа
-    (выбор производится передачей дополнительного аргумента)
-    >>> filter_numbers([1, 2, 3], ODD)
-    <<< [1, 3]
-    >>> filter_numbers([2, 3, 4, 5], EVEN)
-    <<< [2, 4]
-    """
+def filter_numbers(numbers, position):
+    a=[]
+    if position='ODD':
+        for i in numbers:
+            if i%2==1:
+                a.append(i)
+    elif position='EVEN':
+        for i in numbers:
+            if i % 2 == 0:
+                a.append(i)
+    else:
+        for i in numbers:
+            for d in range(2,i):
+                if i%d!=0:
+                    t=+1
+            if t!=0:
+
+
+    return a
+
